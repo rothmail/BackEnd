a50527cdc2c6831dc4811fdd06abbf166bc7709d
Amanda Rothmann <162368529+rothmail@users.noreply.github.com>
2025-06-06 13:52:42 -0300

    Delete Aulas directory

--- Aulas/Aula03/src/models/Usuario.ts
+++
@@ -1,5 +0,0 @@
-export class Usuario {
-  constructor(public id: number, public nome: string, public email: string) {}
-}
-
-export const usuarios: Usuario[] = []; // Simulando um "banco de dados" temporário
