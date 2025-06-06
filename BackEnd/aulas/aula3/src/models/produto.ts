46e2caa07840b513a541cc5668ce2463be2b55d1
Amanda Rothmann <162368529+rothmail@users.noreply.github.com>
2025-06-06 14:06:07 -0300

    Delete BackEnd directory

--- BackEnd/aulas/aula3/src/models/produto.ts
+++
@@ -1,6 +0,0 @@
-export class produto {
-   
-    constructor(public id: number, public nome: string, public preco: Number) {}
-     }
-     
-     export const produtos: produto[] = []; // Simulando um "banco de dados" temporário
