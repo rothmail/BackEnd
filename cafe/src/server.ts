76302695aca8606e7d33f1373c1bfff27ed93521
AMANDA ROTHMANN PIOVESANA <04566835006@senacrs.edu.br>
2025-06-25 08:34:18 -0300

    last update || i wish

--- cafe/src/server.ts
+++ cafe/src/server.ts
@@ -1,1 +1,35 @@
-// server.ts (resumido)
+import express, { Application, Request, Response } from 'express';
+import { AppDataSource } from './database/data-source';
+import routesUser from './routes/RoutesUser';
+import livroRoutes from './routes/RoutesCoffee'; // ou o nome do seu arquivo
+
+import cors from "cors"
+import path from 'path';
+
+const app: Application = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500'
+    ]
+}));
+
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../public/entrar.html"));
+});
+
+AppDataSource.initialize()
+    .then(() => {
+        app.use('/api', routesUser);
+        app.use(livroRoutes);
+        app.listen(3000, () => console.log('Server rodando na porta 3000'));
+    })
+    .catch((error) => console.log(error));
